a796e8b65be4904f71538b90527c6fb7928ae5a8
Lucas <lucasvalenteds@gmail.com>
2021-07-08 22:32:11 -0300

    Configuring logging with Log4J

--- src/main/java/com/portscanner/Main.java
+++ src/main/java/com/portscanner/Main.java
@@ -1,8 +1,13 @@
 package com.portscanner;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 public class Main {
 
+    private static final Logger LOGGER = LogManager.getLogger(Main.class);
+
     public static void main(String[] args) {
-        System.out.println("Main");
+        LOGGER.info("Main");
     }
 }
