083ba999b3e88e734a1198f5889a784f7cc5d130
Lucas <lucasvalenteds@gmail.com>
2021-07-13 13:40:53 -0300

    Using Report.Console in Main

--- src/main/java/com/portscanner/Main.java
+++ src/main/java/com/portscanner/Main.java
@@ -1,7 +1,6 @@
 package com.portscanner;
 
 import picocli.CommandLine;
-import picocli.CommandLine.Help.Ansi;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
@@ -50,61 +49,31 @@ public final class Main implements Callable<Integer> {
     public Integer call() {
         var network = new Network.Local();
         var portScanner = new PortScanner(network);
+        var report = new Report.Console(System.out);
 
         var portScannerResult = portScanner.scan(rangeStart, rangeEnd)
-            .doFirst(this::printPortRange)
+            .doFirst(() -> report.printPortRange(rangeStart, rangeEnd))
             .delayUntil(result ->
                 Flux.from(result.getUnavailable())
-                    .doFirst(this::printUnavailableHeader)
-                    .doOnNext(this::printUnavailablePort)
+                    .doFirst(report::printUnavailableHeader)
+                    .doOnNext(report::printUnavailablePort)
             )
             .filter(result -> showAvailable)
             .delayUntil(result ->
                 Flux.from(result.getAvailable())
-                    .doFirst(this::printAvailableHeader)
-                    .doOnNext(this::printAvailablePort)
+                    .doFirst(report::printAvailableHeader)
+                    .doOnNext(report::printAvailablePort)
             );
 
         return Mono.from(portScannerResult)
             .then(Mono.just(EXIT_CODE_SUCCESS))
             .onErrorReturn(EXIT_CODE_ERROR)
-            .doOnTerminate(this::clearOutputBuffer)
+            .doOnTerminate(report::clearOutputBuffer)
             .block();
     }
 
     public static void main(String[] args) {
         System.exit(new CommandLine(new Main())
             .execute(args));
-    }
-
-    private void printPortRange() {
-        System.out.println(
-            Ansi.Style.reset.on()
-                + "Scanning ports from "
-                + Ansi.Style.bold.on() + rangeStart
-                + Ansi.Style.reset.on() + " to "
-                + Ansi.Style.bold.on() + rangeEnd
-                + Ansi.Style.reset.on() + " ..."
-        );
-    }
-
-    private void printAvailableHeader() {
-        System.out.println("\n" + Ansi.Style.fg_white.on() + "Available ports:");
-    }
-
-    private void printUnavailableHeader() {
-        System.out.println("\n" + Ansi.Style.fg_white.on() + "Unavailable ports:");
-    }
-
-    private void printAvailablePort(int port) {
-        System.out.println(Ansi.Style.reset.on() + "\t* " + Ansi.Style.fg_green.on() + port);
-    }
-
-    private void printUnavailablePort(int port) {
-        System.out.println(Ansi.Style.reset.on() + "\t* " + Ansi.Style.fg_red.on() + port);
-    }
-
-    private void clearOutputBuffer() {
-        System.out.println(Ansi.Style.reset.on());
     }
 }
